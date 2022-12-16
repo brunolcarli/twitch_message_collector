51d6b9bd81d7043dec0514f5b46d2b5bc65ad0a1
Bruno L. Carli <brunolcarli@gmail.com>
2022-12-16 18:23:34 -0300

    ADD message count rank query

--- artopendoragon/schema.py
+++ artopendoragon/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from artopendoragon.models import ChatMessage
+from artopendoragon.analytics import get_messages_count_rank
 
 
 class ChatMessageType(graphene.ObjectType):
@@ -9,6 +10,16 @@ class ChatMessageType(graphene.ObjectType):
     message_sentiment = graphene.Float()
     message_offense_level = graphene.Float()
 
+
+class MessageCountRank(graphene.ObjectType):
+    username = graphene.String()
+    messages_count = graphene.Int()
+
+    def resolve_username(self, info, **kwargs):
+        return self[0]
+
+    def resolve_messages_count(self, info, **kwargs):
+        return self[1]
 
 
 class Query(graphene.ObjectType):
@@ -20,3 +31,7 @@ class Query(graphene.ObjectType):
     chat_messages = graphene.List(ChatMessageType)
     def resolve_chat_messages(self, info, **kwargs):
         return ChatMessage.objects.filter(**kwargs)
+
+    top_ten = graphene.List(MessageCountRank)
+    def resolve_top_ten(self, info, **kwargs):
+        return get_messages_count_rank(ChatMessage.objects.filter(**kwargs))
